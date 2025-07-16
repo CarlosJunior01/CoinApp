30a090db11e3abd00945752c86295bd65501b4c8
carlos.junior <carlos.udi@live.com>
2025-07-16 01:57:45 -0300

    Update(dependencies): adicionar Koin, Retrofit e OkHttp logging interceptor
    - Adicionado Koin para configuração de injeção de dependência
    - Incluído Retrofit e conversor Gson para integração com APIs
    - Integrado OkHttp logging interceptor para log das requisições e respostas
    - Atualizado o libs.versions.toml com referências de versão e declarações de bibliotecas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,15 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+    implementation(libs.koin.compose)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.okhttp.logging)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
